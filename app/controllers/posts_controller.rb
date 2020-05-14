55f87a93c0edb2acea6a222aaf8ecea719779796
louis baptiste <louisvis@ymail.com>
2020-05-14 03:39:17 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @post.to_json(only: [:description, :id, :title] }
+      format.json { render json: @post.to_json(only: [:description, :id, :title]) }
   end
 end
 
